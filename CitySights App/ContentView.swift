9145f6b835d18030ac697a17a057de3a87a44fe1
vikshitkode <kodevikshit2000@gmail.com>
2025-03-11 20:52:11 -0700

    - add config - add gitignore - add api-key variable to plist

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var query: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, stranger!")
+        HStack {
+            TextField("What are you looking for?", text: $query).textFieldStyle(.roundedBorder)
+            Button {
+                // TODO
+            } label: {
+                Text("Go")
+            }
+            
         }
         .padding()
+        .onAppear(perform: {print(Bundle.main.infoDictionary?["API_KEY"] as? String)})
     }
 }
 
