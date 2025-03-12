6f9efc3950bc1ac12480eaf8edd34d5785b6372f
vikshitkode <kodevikshit2000@gmail.com>
2025-03-11 23:25:50 -0700

    - build list UI in Contentview - display returned businesses in list

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -9,22 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var businesses = [Business]()
     @State private var query: String = ""
     var service = DataService()
     
     var body: some View {
-        HStack {
-            TextField("What are you looking for?", text: $query).textFieldStyle(.roundedBorder)
-            Button {
-                // TODO
-            } label: {
-                Text("Go")
+        VStack{
+            HStack {
+                TextField("What are you looking for?", text: $query).textFieldStyle(.roundedBorder)
+                Button {
+                    // TODO
+                } label: {
+                    Text("Go")
+                }
+                
             }
-            
+            List (businesses) { b in
+                Text(b.name ?? "Nil")}
         }
         .padding()
         .task {
-            let businesses = await service.buinessSearch()
+            businesses = await service.buinessSearch()
         }
     }
 }
